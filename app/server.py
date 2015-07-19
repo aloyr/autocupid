e1f139891795ab91c0092c28665475619aa402da
Peter Carrero <peter.carrero@gmail.com>
2015-07-19 13:31:26 -0500

    Finish app API

--- app/server.py
+++ app/server.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template
+from vsn import VSN
 
 app = Flask(__name__)
 
-@app.route('/')
+vsn = VSN()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('kitchen_sink.html')
+  return render_template('index.html')
 
 @app.route('/welcome')
 def welcome():
   return render_template('welcome.html')
 
-@app.route('/testpage')
-def testpage():
-  return render_template('test.html')
+@app.route('/data')
+def datatest():
+	return vsn.getTable()
+
+@app.route('/search/<vsnid>')
+def searchVSN(vsnid):
+	return render_template('search.html', matches=vsn.search(vsnid))
 
 if __name__ == '__main__':
   app.run(debug=True)
